ad1d8b37c09d874fb4c8c4cd6706a3a951de1cf7
Daniel Kucal <dkucal@gmail.com>
2018-04-10 15:17:36 +0200

    feat(EscModule): create module

---
+++ src/esc.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { EscDirective } from './esc.directive';
+
+@NgModule({
+  imports: [],
+  exports: [EscDirective],
+  declarations: [EscDirective],
+  providers: [],
+})
+export class EscModule {}
